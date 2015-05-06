e099941a26b5b64a99ab17a9aaa234956cd9ee7e
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-05-06 13:40:43 -0700

    add compat

---
+++ jupyterdrive/compat.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, absolute_import 
+
+try: 
+    import  jupyter_notebook.nbextensions as nbe
+    JUPYTER=True
+except ImportError:
+    import IPython.html.nbextensions as nbe
+    JUPYTER=False
